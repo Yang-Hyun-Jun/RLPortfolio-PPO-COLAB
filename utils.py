16d1b0ac70241f61cac0cbdbca28d5f0c948add9
Hyun Jun <eppioes@gmail.com>
2022-06-10 13:57:50 +0900

    for GPU

--- utils.py
+++ utils.py
@@ -1,17 +1,3 @@
-import numpy as np
-
 Base_DIR = "/Users/mac/Desktop/OHLCV_data/ALL_OHLCV"
-SAVE_DIR = "/Users/mac/Desktop/RLPortfolio/DirichletPortfolio"
+SAVE_DIR = "/content"
 
-def sigmoid(x):
-    x = np.clip(x, -10, 10)
-    return 1. / (1. + np.exp(-x))
-
-def exp(x):
-    return np.exp(x)
-
-if __name__ == "__main__":
-    a = np.array([1, 2, 3])
-    b = np.array([1, 2, 5])
-
-    print( a == b )
